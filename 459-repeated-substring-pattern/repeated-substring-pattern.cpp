92d235b6950b6e237a39e429fbe6a370461c0b7f
Priyanshu Naik <anshu17naik@gmail.com>
2025-08-05 08:08:34 +0530

    Time: 5 ms (72.80%) | Memory: 14.9 MB (85.75%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t = s + s;
+        string trimmed = t.substr(1, t.size()-2);
+        return trimmed.find(s) != string::npos;
+    }
+};
