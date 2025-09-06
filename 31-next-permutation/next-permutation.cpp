726e1a72d3c079572254f7cbcfc0f48794d0ff36
Priyanshu Naik <anshu17naik@gmail.com>
2025-09-07 01:07:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (67.72%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,10 +3,15 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int i = n-2;
+        //traverse from right
         while(i >= 0 && nums[i] >= nums[i+1]){
             i--;
         }
-
+        //When we run the loop:
+		//Start from i = n-2 = 1 (nums[1] = 1).
+		//Compare nums[1] (1) with nums[2] (5).
+		//Since 1 < 5, the condition nums[i] >= nums[i+1] is false → loop stops.
+        //So the pivot is at index 1 (0-based).
         if(i >= 0){
             int j = n-1;
             while(nums[j] <= nums[i]){
