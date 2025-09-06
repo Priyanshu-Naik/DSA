1fe4c7e5493e1936c92f3ed73248a3c99c3bc13e
Priyanshu Naik <anshu17naik@gmail.com>
2025-09-06 20:52:12 +0530

    Time: 35 ms (89.35%) | Memory: 45.5 MB (43.10%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixFreq;
-        prefixFreq[0] = 1; // base case for sum = k starting from index 0
-
-        int count = 0;
-        int sum = 0;
-
-        for (int num : nums) {
-            sum += num;
-
-            // Check if there's a prefix sum that makes (sum - k)
-            if (prefixFreq.find(sum - k) != prefixFreq.end()) {
-                count += prefixFreq[sum - k];
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int prefixsum = 0, count = 0;
+        for(int num : nums){
+            prefixsum += num;
+            if(mp.find(prefixsum - k) != mp.end()){
+                count += mp[prefixsum - k];
             }
-
-            // Record current sum
-            prefixFreq[sum]++;
+            mp[prefixsum]++;
         }
-
         return count;
     }
 };
