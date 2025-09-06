163291a6b7f310db3bd6cac3ac3508db1f79a203
Priyanshu Naik <anshu17naik@gmail.com>
2025-09-07 00:24:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (33.05%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,30 +2,18 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int ind = -1;
-        // Step 1: Find the first index from the right where arr[i] < arr[i + 1]
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                ind = i;
-                break;
+        int i = n-2;
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+
+        if(i >= 0){
+            int j = n-1;
+            while(nums[j] <= nums[i]){
+                j--;
             }
+            swap(nums[i], nums[j]);
         }
-        // Step 2: If no such index, it's the last permutation → reverse to
-        // first
-        if (ind == -1) {
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-        // Step 3: Find the smallest element on the right of 'ind' that is
-        // greater than arr[ind]
-        for (int i = n - 1; i > ind; i--) {
-            if (nums[i] > nums[ind]) {
-                swap(nums[i], nums[ind]);
-                break;
-            }
-        }
-        // Step 4: Reverse the subarray to the right of 'ind'
-        reverse(nums.begin() + ind + 1, nums.end());
-        return;
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
