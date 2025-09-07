8cbfb9bb1cf66d8d29d233d8c1a033d1cd8c9d3f
Priyanshu Naik <anshu17naik@gmail.com>
2025-09-07 11:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.96%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    vector<int> spiralOrder(const vector<vector<int>>& matrix) {
-        vector<int> ans_3;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int top = 0, bottom = n - 1, left = 0, right = m - 1;
-
-        while (top <= bottom && left <= right) {
-            for (int i = left; i <= right; i++) {
-                ans_3.push_back(matrix[top][i]);
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        if(matrix.empty()) return res;
+        int top = 0, bottom = matrix.size() - 1;
+        int left = 0, right = matrix[0].size() - 1;
+        while(top <= bottom && left <= right){
+            //left->right
+            for(int j = left; j <= right; j++){
+                res.push_back(matrix[top][j]);
             }
             top++;
 
-            for (int i = top; i <= bottom; i++) {
-                ans_3.push_back(matrix[i][right]);
+            //top->bottom
+            for(int i = top; i <= bottom; i++){
+                res.push_back(matrix[i][right]);
             }
             right--;
 
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    ans_3.push_back(matrix[bottom][i]);
+            //right->left
+            if(top <= bottom){
+                for(int j = right; j >= left; j--){
+                    res.push_back(matrix[bottom][j]);
                 }
                 bottom--;
             }
 
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans_3.push_back(matrix[i][left]);
+            //bottom->top;
+            if(left <= right){
+                for(int i = bottom; i >= top; i--){
+                    res.push_back(matrix[i][left]);
                 }
                 left++;
             }
         }
-
-        return ans_3;
+        return res;
     }
 };
