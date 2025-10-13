724d2b6fdc389d013dfe4a15b194c1d49631c20f
Priyanshu Naik <anshu17naik@gmail.com>
2025-10-13 08:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.36%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {//two pointer approach
-        int insertPos = 0;
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
         int n = nums.size();
-        //insert the non-zero elements to the start one by one
         for(int i = 0; i < n; i++){
             if(nums[i] != 0){
-                nums[insertPos] = nums[i];
-                insertPos++;
+                swap(nums[i], nums[count]);
+                count++;
             }
-        }
-        //Make the remaining elements as zeros
-        while(insertPos < n){
-            nums[insertPos] = 0;
-            insertPos++;
         }
     }
 };
